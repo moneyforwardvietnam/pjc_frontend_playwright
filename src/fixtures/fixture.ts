50cee1d92d2b01db7aabe709af8457ba982ae6e0
Nguyễn Trọng Vĩnh (Vin) <nguyen.trong.vinh@moneyforward.vn>
2023-12-16 14:22:40 +0700

    Support authentication fixture

--- src/fixtures/fixture.ts
+++ src/fixtures/fixture.ts
@@ -1,4 +1,3 @@
-import { accounts } from '@data/accounts'
 import { LoginPage } from '@pages/login-page'
 import { SwitchOfficePage } from '@pages/offices/switch-office-page'
 import { test as baseTest } from '@playwright/test'
@@ -6,34 +5,35 @@ import fs from 'fs'
 import path from 'path'
 export * from '@playwright/test'
 
-export const test = baseTest.extend<{}, { workerStorageState: string }>({
-  // Use the same storage state for all tests in this worker.
-  storageState: ({ workerStorageState }, use) => use(workerStorageState),
+export const test = baseTest.extend<{}, { authentication?: {
+    email: string,
+    password: string,
+    officeName: string,
+  }}>({
+  context: async ({authentication, browser}, use) => {
+    if (!authentication) return
 
-  // Authenticate once per worker with a worker-scoped fixture.
-  workerStorageState: [
-    async ({ browser }, use) => {
-      // Use parallelIndex as a unique identifier for each worker.
-      const id = test.info().parallelIndex
-      const fileName = path.resolve(`src/.auth/user${id}.json`)
+    const md5 = require('crypto').createHash('md5').update(authentication.email + authentication.officeName).digest('hex')
+    const fileName = path.resolve(`src/.auth/user-${md5}.json`)
 
-      if (fs.existsSync(fileName)) {
-        // Reuse existing authentication state if any.
-        await use(fileName)
-        return
-      }
+    if (fs.existsSync(fileName)) {
+      const context = await browser.newContext({storageState: fileName})
+      return use(context)
+    }
 
-      // Important: make sure we authenticate in a clean environment by unsetting storage state.
-      const page = await browser.newPage({ locale: 'ja', storageState: undefined })
-      const pageLogin = new LoginPage(page)
+    const page = await browser.newPage({ locale: 'ja', storageState: undefined })
+    const pageLogin = new LoginPage(page)
 
-      await pageLogin.login(accounts.DEFAULT.email, accounts.DEFAULT.password)
-      const switchOfficePage = new SwitchOfficePage(page)
-      await switchOfficePage.switchOffice('QA')
-      await page.context().storageState({ path: fileName })
-      await page.close()
-      await use(fileName)
-    },
-    { scope: 'worker' },
-  ],
+    await pageLogin.login(authentication.email, authentication.password)
+    const switchOfficePage = new SwitchOfficePage(page)
+    await switchOfficePage.switchOffice(authentication.officeName)
+    const context = page.context()
+    await context.storageState({ path: fileName })
+    await page.close()
+
+    const browserContext = await browser.newContext({storageState: fileName})
+    return use(browserContext)
+  }
 })
+
+export const expect = test.expect
