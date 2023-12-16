31ab6e3ffe35a4985965ad6f028ce183b20b4c71
Nguyễn Trọng Vĩnh (Vin) <nguyen.trong.vinh@moneyforward.vn>
2023-12-16 14:22:54 +0700

    Fix test for login & switch office

--- src/pages/login-page.ts
+++ src/pages/login-page.ts
@@ -3,7 +3,7 @@ import { Locator, Page } from '@playwright/test'
 
 export class LoginPage extends PageCommon {
   public loginUrl: string
-  public expectUrlAfterLogin: string
+  public expectUrlAfterLogin: string = '/'
   private readonly emailInput: Locator
   private readonly passwordInput: Locator
   private signInButton: Locator
@@ -12,8 +12,8 @@ export class LoginPage extends PageCommon {
     super(page)
     this.loginUrl = '/login'
     this.emailInput = page.getByPlaceholder('example@moneyforward.com')
-    this.passwordInput = page.getByLabel('Password', { exact: true })
-    this.signInButton = page.getByRole('button', { name: 'Sign in' })
+    this.passwordInput = page.getByLabel('パスワード', { exact: true })
+    this.signInButton = page.getByRole('button', { name: 'ログインする', exact: true })
   }
 
   async goto() {
@@ -23,7 +23,7 @@ export class LoginPage extends PageCommon {
   async login(email: string, password: string) {
     await this.goto()
     await this.clickLocatorByTestId('login-button')
-    const availableAccountButton = await this.page.getByText('Use other accounts')
+    const availableAccountButton = this.page.getByText('Use other accounts')
     if ((await availableAccountButton.count()) > 0) {
       await availableAccountButton.click()
     }
@@ -32,11 +32,12 @@ export class LoginPage extends PageCommon {
     await this.signInButton.click()
     await this.passwordInput.fill(password)
     await this.signInButton.click()
-    await this.page.waitForTimeout(2000)
     const btnSkip = this.page.getByText('No thanks')
     if ((await btnSkip.count()) > 0) {
       await btnSkip.click()
     }
-    await this.page.waitForURL(this.expectUrlAfterLogin)
+
+    console.log('this.expectUrlAfterLogin', this.expectUrlAfterLogin);
+    await this.page.waitForURL(this.expectUrlAfterLogin, {waitUntil: 'domcontentloaded'})
   }
 }
--- src/pages/offices/switch-office-page.ts
+++ src/pages/offices/switch-office-page.ts
@@ -1,3 +1,4 @@
+import { expect } from "@fixtures/fixture";
 import { PageCommon } from '@pages/page-common'
 import { Page, test } from '@playwright/test'
 import { envConfig } from '@utils/envConfig'
@@ -12,31 +13,32 @@ export class SwitchOfficePage extends PageCommon {
   }
 
   async goto() {
-    await this.page.goto(envConfig.baseUrl + 'offices/switch', { waitUntil: 'networkidle' })
+    await this.page.goto(envConfig.baseUrl + 'offices/switch', {waitUntil: 'networkidle'})
   }
 
   async switchOffice(officeName: string) {
     await this.goto()
-    const selectButton = this.page.getByRole('row', { name: officeName })
-    const isSelectedOfficeDisables = await this.page
+
+    await this.page.waitForLoadState('domcontentloaded')
+
+    const selectOfficeButtonPromise = this.page
       .getByRole('row', { name: officeName })
       .getByRole('button', { name: '切替' })
-      ?.isDisabled()
-    if (isSelectedOfficeDisables) {
+
+    const isDisabled = await selectOfficeButtonPromise.isDisabled( )
+    if (isDisabled) {
       await this.page.goto(envConfig.baseUrl)
       return
     }
-    await this.page.getByRole('row', { name: officeName }).getByRole('button', { name: '切替' }).click()
+
+    await selectOfficeButtonPromise.click()
     await this.page.waitForURL(envConfig.baseUrl)
-    const id = test.info().parallelIndex
-    const fileName = path.resolve(`src/.auth/user${id}.json`)
-    await this.page.context().storageState({ path: fileName })
   }
 
   async switchOfficeByOfficeId(officeId: string) {
     await this.goto()
     const selectedButton = await this.page
-      .locator('tbody.ant-table-tbody tr[data-row-key="01GHZJKJGPKMZ5Z38K0Y34J89N"]')
+      .locator(`tbody.ant-table-tbody tr[data-row-key="${officeId}"]`)
       ?.getByRole('button', { name: '切替' })
     if (await selectedButton.isDisabled()) {
       await this.page.goto(envConfig.baseUrl)
