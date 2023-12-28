462b7b57b0a770299a233c116d89cc9c190947fa
Scott <nguyen.tai.thanh@moneyforward.vn>
2023-12-28 21:40:22 +0700

    Update test case login again

--- e2e/common/C24036 Login again after logging out.spec.ts
+++ e2e/common/C24036 Login again after logging out.spec.ts
@@ -1,18 +1,18 @@
-import { accounts } from '@data/accounts'
+import { accountsByEmail } from '@data/accounts'
 import { expect, test } from '@playwright/test'
 import { envConfig } from '@utils/envConfig'
 import { loginFlow } from '../../src/flows/login'
 
 test('C24036 Login again after logging out', async ({ page }) => {
-  await loginFlow(page, accounts.DEFAULT)
+  await loginFlow(page, accountsByEmail['nguyen.thi.ngoc+qa1@moneyforward.vn'].email)
   await expect(page.url()).toBe(`${envConfig.baseUrl}`)
   await page.getByTestId('office-user-name-button').click()
   await page.getByRole('menuitem', { name: 'ログアウト' }).click()
   await page.waitForURL(`${envConfig.baseUrl}login`)
   await expect(page.url()).toBe(`${envConfig.baseUrl}login`)
   await page.getByText('マネーフォワードIDでログインして続行').click()
-  await expect(page.getByText('Use this account')).toBeVisible()
-  await page.getByText('Use this account').click()
+  await expect(page.getByText(accountsByEmail['nguyen.thi.ngoc+qa1@moneyforward.vn'].email)).toBeVisible()
+  await page.getByText(accountsByEmail['nguyen.thi.ngoc+qa1@moneyforward.vn'].email).click()
   await page.waitForURL(`${envConfig.baseUrl}`)
   await expect(page.url()).toBe(`${envConfig.baseUrl}`)
 })
