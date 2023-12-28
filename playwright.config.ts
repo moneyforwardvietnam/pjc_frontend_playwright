2eb651226a05cae90c29b2518b509fa1c4c2c118
Scott <nguyen.tai.thanh@moneyforward.vn>
2023-12-28 20:57:16 +0700

    Update timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,7 @@ import { defineConfig, devices } from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 60*1000,
   testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -31,7 +32,6 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     testIdAttribute: 'data-testid',
-    locale: 'ja-JP',
   },
 
   /* Configure projects for major browsers */
