a71368d9c7d315ac86adcf8a64b3796968a346a4
Scott <nguyen.tai.thanh@moneyforward.vn>
2024-01-02 14:47:00 +0700

    Verify delete a project

--- src/pages/project/project-page.ts
+++ src/pages/project/project-page.ts
@@ -1,12 +1,20 @@
 import { PageCommon } from '@pages/page-common'
 
 export class ProjectPage extends PageCommon {
+  get deleteButton() {
+    return this.page.getByRole('button', { name: '削除' })
+  }
+
   get editButton() {
     return this.page.getByRole('button', { name: '編集' })
   }
 
   get saveButton() {
     return this.page.getByRole('button', { name: '保存' })
+  }
+
+  goto(biid: string) {
+    return this.page.goto(`/project/${biid}`)
   }
 
   waitForProjectLoaded() {
@@ -19,6 +27,12 @@ export class ProjectPage extends PageCommon {
 
   waitForProjectUpdated() {
     return this.page.waitForResponse('**/api/v1/project_master/projects/update/**')
+  }
+
+  waitForProjectDeleted() {
+    return this.page.waitForResponse((response) => {
+      return response.url().includes('/api/v1/project_master/projects/') && response.request().method() === 'DELETE'
+    })
   }
 
   async editProject() {
